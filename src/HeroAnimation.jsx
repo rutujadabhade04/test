c6dd64f3aabe31020224b964c4931892ff85d80e
rutujadabhade04 <168404028+rutujadabhade04@users.noreply.github.com>
2025-07-27 17:37:02 +0530

    Update HeroAnimation.jsx

--- src/HeroAnimation.jsx
+++ src/HeroAnimation.jsx
@@ -44,7 +44,7 @@ export default function HeroAnimation({ NAV_HEIGHT }) {
       }}
     >
       <motion.img
-        src="/public/images/cute_chat.jpeg"
+        src="/test/images/cute_chat.jpeg"
         alt="img"
         style={{
           width: "100%",
